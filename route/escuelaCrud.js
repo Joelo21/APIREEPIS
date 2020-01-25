81f98782af78c628aba7d8ced94cdac7d0dce99e
nidhogg-spec <55214512+nidhogg-spec@users.noreply.github.com>
2020-01-25 13:23:54 -0500

    Update escuelaCrud.js

--- route/escuelaCrud.js
+++ route/escuelaCrud.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const CEscuelaServicio = require("../services/Sescuela");
 const EscuelaServicio = new CEscuelaServicio();
 
-router.get("/",async function(req, res, next ){
+router.get("/List",async function(req, res, next ){
  try 
   {
     const Tescuela = await EscuelaServicio.TraerTodos('CALL USP_MDL_ESCUELA_TT').then(escuela =>{
-      if (escuela[0].length===0) {
+      if (escuela[0].length === 0) {
         res.json({
-          Cod: 0
+          Codigo: 0
         });             
       } else {
         res.status(200).json({
           Escuela: escuela[0],
-          Mensaje:'Escuelas listadas'
+          Codigo: 1
       });
       }
     });  
@@ -31,64 +31,65 @@ router.get('/:Cod_Escuela',async function(req,res,next)
   try
   {
     const TUescuela = await EscuelaServicio.TraerUno("CALL USP_MDL_ESCUELA_TU(?)",[Cod_Escuela]).then(escuela=>{
-      if (escuela[0].length===0) {
+      if (escuela[0].length === 0) {
         res.json({
-          Cod: 0
+          Codigo: 0
         });       
       } else {
         res.status(200).json({
           Escuela: escuela[0][0],
-          Cod: 1
+          Codigo: 1
         });
       }
-    });
-  
+    });  
   }
-    catch(err)
-    {
-      next(err);
-    }
+  catch(err)
+  {
+    next(err);
+  }
 });
 
 router.post('/Guardar',async function(req,res,next)
 {
   const {Cod_Escuela,Sede,Descripcion}=req.body;
-
-    try
-    {
-      const Gescuela = await EscuelaServicio.Guardar("CALL USP_MDL_ESCUELA_G(?,?,?)",[Cod_Escuela,Sede,Descripcion]).then(escuela =>{
-          res.status(201).json({
-            Escuela: escuela,
-            Mensaje: 'Escuela guardada'
-          });
-        });
-    }
-    catch(err)
-    {
-      next(err);
-    }
+  try
+  {
+    const Gescuela = await EscuelaServicio.Guardar("CALL USP_MDL_ESCUELA_G(?,?,?)",[Cod_Escuela,Sede,Descripcion]).then(escuela =>{
+      res.status(201).json({
+        Escuela: escuela,
+        Codigo: 'Escuela guardada'
+      });
+      console.log(escuela);         
+    });
+  }
+  catch(err)
+  {
+    next(err);
+  }
 });
 
-
-router.delete('/:Cod_Escuela',async function(req,res,next)
+router.delete('/Del:Cod_Escuela',async function(req,res,next)
 {
   const {Cod_Escuela}=req.params;
-
   try
   {
     const Eescuela  = await EscuelaServicio.Eliminar('CALL USP_MDL_ESCUELA_E(?)',[Cod_Escuela]).then(escuela =>{
-      res.status(201).json({
-        Resultado: escuela,
-        message: 'Escuela eliminada'
-      });
+      if (escuela.affectedRows === 0) {
+        res.json({
+          Codigo: 0
+        });
+      } else {
+        res.status(201).json({
+          Escuela: 'Escuela Eliminada',
+          Codigo: 1
+        });
+      }
     })
   }
-    catch(err)
-    {
-      next(err);
-    }
+  catch(err)
+  {
+    next(err);
+  }
 });
-
-
 
 module.exports = router;
