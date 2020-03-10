26540bd815fbd33c78caf2a4e7834284cdd4c689
Joelo <49916469+joesu21@users.noreply.github.com>
2020-03-09 22:25:03 -0500

    tablas y SP

--- TABLAS.sql
+++ TABLAS.sql
@@ -137,3 +137,68 @@ BEGIN
    where Cod_CursoObjetivo=pCod_CursoObjetivo;
    END//
 DELIMITER ;
+
+-- Volcando estructura para tabla reepis.mdl_IndRecursobjetivo
+CREATE TABLE IF NOT EXISTS `mdl_IndRecursobjetivo` (
+  `Cod_Indrecursobjetivo` varchar(8) NULL,
+  `Cod_ResulCursOjetivo` varchar(8)  NOT NULL,
+  PRIMARY KEY (`Cod_Indrecursobjetivo`,`Cod_ResulCursOjetivo`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+
+-- Volcando estructura para procedimiento reepis.USP_MDL_IndRecursobjetivo_E
+DELIMITER //
+CREATE PROCEDURE `USP_MDL_IndRecursobjetivo_E`(
+   IN pCod_Indrecursobjetivo VARCHAR(8)
+   )
+BEGIN
+   DELETE FROM mdl_IndRecursobjetivo
+   WHERE (Cod_Indrecursobjetivo = pCod_Indrecursobjetivo);
+END//
+DELIMITER ;
+
+-- Volcando estructura para procedimiento reepis.USP_MDL_IndRecursobjetivo_G
+DELIMITER //
+CREATE PROCEDURE `USP_MDL_IndRecursobjetivo_G`(
+IN pCod_Indrecursobjetivo VARCHAR(8),
+IN pCod_ResulCursOjetivo VARCHAR(8)
+)
+BEGIN
+IF NOT EXISTS (SELECT Cod_Indrecursobjetivo FROM mdl_IndRecursobjetivo WHERE Cod_Indrecursobjetivo = pCod_Indrecursobjetivo)
+THEN
+INSERT INTO mdl_IndRecursobjetivo(
+Cod_Indrecursobjetivo,
+Cod_ResulCursOjetivo
+)
+VALUES (
+pCod_Indrecursobjetivo,
+pCod_ResulCursOjetivo
+);
+ELSE
+UPDATE mdl_IndRecursobjetivo
+SET
+Cod_Indrecursobjetivo=pCod_Indrecursobjetivo,
+Cod_ResulCursOjetivo=pCod_ResulCursOjetivo
+WHERE (Cod_Indrecursobjetivo=pCod_Indrecursobjetivo);
+END IF;
+END//
+DELIMITER ;
+
+-- Volcando estructura para procedimiento reepis.USP_MDL_IndRecursobjetivo_TT
+DELIMITER //
+CREATE PROCEDURE `USP_MDL_IndRecursobjetivo_TT`()
+BEGIN
+	SELECT * FROM mdl_IndRecursobjetivo;
+	END//
+DELIMITER ;
+
+-- Volcando estructura para procedimiento reepis.USP_MDL_IndRecursobjetivo_TU
+DELIMITER //
+CREATE PROCEDURE  `USP_MDL_IndRecursobjetivo_TU`(
+   IN pCod_Indrecursobjetivo INT)
+BEGIN
+   SELECT *
+   FROM mdl_IndRecursobjetivo
+   where Cod_Indrecursobjetivo=pCod_Indrecursobjetivo;
+   END//
+DELIMITER ;
